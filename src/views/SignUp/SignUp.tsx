26aa66c40d68893a6916ad4703e55f1990dd81c0
ChloeSAPage <chloesapagepersonal@gmail.com>
2024-09-16 20:47:47 +0100

    Styling on form

--- src/views/SignUp/SignUp.tsx
+++ src/views/SignUp/SignUp.tsx
@@ -3,10 +3,33 @@ import styles from "./SignUp.module.css";
 function SignUp() {
     return (
         <div className={styles.wrapper}>
-            <p>Sign Up</p>
-            <form>
-                <input type="text" />
-            </form>
+            <div className={styles.container}>
+                <form className={styles.form}>
+                    <input
+                        className={styles.input}
+                        type="text"
+                        id="username"
+                        name="username"
+                        placeholder="Username"
+                        required
+                    />
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="Email"
+                        required
+                    />
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="Password"
+                        required
+                    />
+                    <button type="button">Sign Up</button>
+                </form>
+            </div>
         </div>
     );
 }
